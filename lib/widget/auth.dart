0f6044e8326651b84e40bb4f2e318913ec1728a0
Sridhar <sridharonstar@gmail.com>
2023-09-05 16:29:04 +0530

    add the validate

--- lib/widget/auth.dart
+++ lib/widget/auth.dart
@@ -11,11 +11,19 @@ class AuthScreeen extends StatefulWidget {
 
 class _AuthScreeenState extends State<AuthScreeen> {
   var _isLogin = true;
-  // ignore: unused_field
-  final _formkey = GlobalKey<FormState>();
 
-  void _submit(){
+  var _enteredEmail = '';
+  var _enteredPassword = '';
 
+  final _form = GlobalKey<FormState>();
+
+  void _submit() {
+   final isValid = _form.currentState!.validate();
+   if(isValid){
+    _form.currentState!.save();
+    print(_enteredEmail);
+    print(_enteredPassword);
+   }
   }
   @override
   Widget build(BuildContext context) {
@@ -38,6 +46,7 @@ class _AuthScreeenState extends State<AuthScreeen> {
                 padding: const EdgeInsets.only(
                     left: 10, top: 0, bottom: 10, right: 10),
                 child: Form(
+                  key: _form,
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
@@ -52,24 +61,28 @@ class _AuthScreeenState extends State<AuthScreeen> {
                           if (value == null ||
                               value.trim().isEmpty ||
                               !value.contains('@')) {
-                                return 'Please enter a valid email address.';
-                              }
-                              return null;
+                            return 'Please enter a valid email address.';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _enteredEmail = value!;
                         },
                       ),
                       TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: "Password",
-                        ),
-                        obscureText: true, //invisiable the txt
-                        validator: (value) {
-                          if (value == null ||
-                              value.trim().length < 6) {
-                                return 'Password must be atleast 6 charaters long ';
-                              }
-                              return null;
-                        }
-                      ),
+                          decoration: const InputDecoration(
+                            labelText: "Password",
+                          ),
+                          obscureText: true, //invisiable the txt
+                          validator: (value) {
+                            if (value == null || value.trim().length < 6) {
+                              return 'Password must be atleast 6 charaters long ';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                          _enteredPassword = value!;
+                        },),
                       const SizedBox(height: 12),
                       ElevatedButton(
                         onPressed: _submit,
